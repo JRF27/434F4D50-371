935098ece5ec36795cbb1038d72893ac7d2c08e3
JRF27 <jrfriess@gmail.com>
2017-10-07 12:01:14 -0400

    Reverted an incorrect change.

--- A1/Main.cpp
+++ A1/Main.cpp
@@ -10,7 +10,8 @@
 #include "glm.hpp"
 #include "gtc/matrix_transform.hpp"
 #include "gtc/type_ptr.hpp"
-#include <vector>
+
+#include "objloader.hpp"					// include the object loader
 
 using namespace std;
 
