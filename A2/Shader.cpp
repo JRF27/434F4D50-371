3f4c32b7c43a866769cad33f4af9aaf29dfcff45
JRF27 <jrfriess@gmail.com>
2017-10-07 15:41:03 -0400

    Object-oriented shaders.

---
+++ A2/Shader.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "..\glew\glew.h" // include GL Extension Wrangler
+
+#include "Shader.hpp"
+
+Shader::Shader(std::string fileName, const int type)
+{
+	m_path = fileName;
+	m_type = type;
+
+	std::ifstream shaderStream(m_path, std::ios::in);
+
+	if (shaderStream.is_open())
+	{
+		std::string Line = "";
+		while (getline(shaderStream, Line))
+			m_code += "\n" + Line;
+		shaderStream.close();
+	}
+	else
+	{
+		printf("Impossible to open %s. Are you in the right directory ?\n", m_path.c_str());
+		getchar();
+		exit(-1);
+	}
+}
+
+Shader::~Shader()
+{
+
+}
+
+GLuint Shader::Compile()
+{
+	GLuint shader = glCreateShader(m_type);
+	char const * sourcePointer = m_code.c_str();
+	glShaderSource(shader, 1, &sourcePointer, NULL);
+	glCompileShader(shader);
+	// Check for compile time errors
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	return shader;
+}
---
+++ A2/ShaderProgram.hpp
@@ -0,0 +1,15 @@
+#ifndef SHADER_PROGRAM_H
+#define SHADER_PROGRAM_H
+
+class ShaderProgram {
+
+public:
+	ShaderProgram(std::string vertexPath, std::string fragmentPath);
+
+	void Run();
+	unsigned int id() { return m_id; };
+
+private:
+	unsigned int m_id;
+};
+#endif SHADER_PROGRAM_H
